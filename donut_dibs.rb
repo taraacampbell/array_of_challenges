713ff4b58513890f5cc58c1baf8540ed07ca084b
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-02 09:38:16 -0600

    added donut dibs

---
+++ donut_dibs.rb
@@ -0,0 +1,2 @@
+donut_box1 = ["Boston Creme", "Boston Creme", "Boston Creme", "Choc Long John", "Choc Long John", "Vanilla Long John", "Vanilla Long John", "Old Fashioned", "Old Fashioned", "Old Fashioned", "French thing", "French Thing"]
+donut_box2 = ["Choc Sprinkles", "Choc Sprinkles", "double choc cake", "double choc cake", "double choc cake", "glazed", "glazed", "glazed", "choc cake", "choc cake", "choc cake"]
