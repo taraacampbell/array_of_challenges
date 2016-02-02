dd2db7dd1739d44134ccb98999dbf7e5e9892d9d
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-02 08:58:55 -0600

    challenge setup

--- array_of_challenges.rb
+++ array_of_challenges.rb
@@ -2,11 +2,12 @@
 	#Sort all the names in this array alphabetically
 	star_students = [ "Tara", "Rahul", "Liza", "Charlie", "Remy", "Dean", "Reis", "Holden", "Andrew"]
 
+puts star_students[2]
 	#create a new array that has 19 elements of only your name as a string
 		#(hint: There is a way do this with only typing the string once)
 
 
-	#That first array is missing students!
+	#That first array, star_students and this array, brilliant_students, should be one big array!
 		#combine this array with the one above
 		#Then sort the final array in alphabetic order
 
@@ -14,7 +15,7 @@
 
 	
 	#BONUS
-		#Iterate through this array, printing each student with an adjective
+		#Iterate through the final student array, printing each student with an adjective
 			#ex:   Dynamic Dean   /n  Witty Andrew  /n  Diplomatic Luke
 
 			adjectives = ["adventurous", "ambitious", "brave", "bright", "charming", "courageous", "creative", "determined", "dynamic", "diplomatic", "enthusiastic", "helpful", "honest", "humorous", "inventive", "persistent", "powerful", "reliable", "resourceful", "sincere", "thoughtful", "witty"]
